6b2c57ba7df17098db0f593f0b202ee1709f1b37
Lokesh-pavan-kumar <lokesh.7.8.kl@gmail.com>
2020-03-10 17:01:55 +0530

    Nothing

--- project/handy/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Artisan, Product, Category
-
-admin.site.register(Artisan)
-admin.site.register(Product)
-admin.site.register(Category)
--- project/handy/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class HandyConfig(AppConfig):
-    name = 'handy'
