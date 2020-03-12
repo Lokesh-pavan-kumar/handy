cff43040a191d34e8959cd65a61d07e2cf8b9aae
unknown <dandamudijagruthi13@gmail.com>
2020-03-12 17:07:40 +0530

    Integrated

---
+++ handy/handy/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from .models import Product, Category
+from django.views.generic import ListView, DetailView
+
+
+def home(request):
+    products = {
+        'thumbnails': Product.objects.all(),
+        'category': Category.objects.all()
+    }
+    return render(request, 'handy/home.html', products)
+
+
+class ProductDetailView(DetailView):
+    model = Product
+
