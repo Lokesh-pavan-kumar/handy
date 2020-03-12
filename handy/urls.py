0fdbdc75fbe219228fe4bffdfaa0a1f99abe0f17
Lokesh-pavan-kumar <lokesh.7.8.kl@gmail.com>
2020-03-12 17:55:45 +0530

    Added artisan page

--- handy/urls.py
+++ handy/urls.py
@@ -1,10 +1,12 @@
 
 from django.urls import path
-from .views import ProductDetailView
+from .views import ProductDetailView, ArtisanDetailView
 from . import views
 
 urlpatterns = [
     path('', views.home, name="handy-home"),
-    path('product/<int:pk>/', ProductDetailView.as_view(), name="product-detail")
+    path('product/<int:pk>/', ProductDetailView.as_view(), name="product-detail"),
+    path('artisan/<int:pk>/', ArtisanDetailView.as_view(), name="artisan-detail")
 ]
+
 
